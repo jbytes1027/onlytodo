03cf3be15afff6b3df65c98c1cec8b575147e47c
James Pretorius <jamesmpretorius@gmail.com>
2023-10-03 23:28:21 -0400

    disable cors

--- Program.cs
+++ Program.cs
@@ -3,14 +3,19 @@ using OnlyTodo.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+{
+    policy.AllowAnyOrigin(); policy.AllowAnyHeader();
+}));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<OnlyTodoContext>();
 builder.Services.AddScoped<TaskService>();
 
 var app = builder.Build();
+app.UseCors();
 app.MapControllers();
 
 app.Run();
 
-public partial class Program {}
+public partial class Program { }
